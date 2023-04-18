a4411807a686c63858549207451e585684f437ed
yuktnk <yuki0wl@gmail.com>
2023-04-18 23:13:15 +0900

    サーバー側のrouterの設定、tRPCの関数の作成

---
+++ src/server/trpc/router/todo.ts
@@ -0,0 +1,72 @@
+import {
+  createTaskSchema,
+  getSingleTaskSchema,
+  updateTaskShchema,
+  deleteTaskSchema,
+} from "../../../schema/todo";
+
+import { t, authedProcedure } from "../trpc";
+
+export const todoRouter = t.router({
+  createTask: authedProcedure
+    .input(createTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.create({
+        data: {
+          ...input,
+          user: {
+            connect: {
+              id: ctx.session?.user?.id,
+            },
+          },
+        },
+      });
+      return task;
+    }),
+
+  getTasks: t.procedure.query(({ ctx }) => {
+    return ctx.prisma.task.findMany({
+      where: {
+        userId: ctx.session?.user?.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }),
+
+  getSingleTask: authedProcedure
+    .input(getSingleTaskSchema)
+    .query(({ ctx, input }) => {
+      return ctx.prisma.task.findUnique({
+        where: {
+          id: input.taskId,
+        },
+      });
+    }),
+
+  updateTask: authedProcedure
+    .input(updateTaskShchema)
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.prisma.task.update({
+        where: {
+          id: input.taskId,
+        },
+        data: {
+          title: input.title,
+          body: input.body,
+        },
+      });
+      return task;
+    }),
+
+  deleteTask: authedProcedure
+    .input(deleteTaskSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.task.delete({
+        where: {
+          id: input.taskId,
+        },
+      });
+    }),
+});
