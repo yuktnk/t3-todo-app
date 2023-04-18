79df0183ceb948632289ec01388d92e623c0a649
yuktnk <yuki0wl@gmail.com>
2023-04-18 23:45:24 +0900

    schemaにコメント追加

--- src/schema/todo.ts
+++ src/schema/todo.ts
@@ -1,19 +1,16 @@
 import z from "zod";
 
+// サーバー側から参照される関数
 export const createTaskSchema = z.object({
   title: z.string().max(20),
   body: z.string().min(5),
 });
-
-export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>;
 
 export const updateTaskShchema = z.object({
   taskId: z.string().cuid(),
   title: z.string().max(20),
   body: z.string().min(5),
 });
-
-export type UpdateTaskInput = z.TypeOf<typeof updateTaskShchema>;
 
 export const getSingleTaskSchema = z.object({
   taskId: z.string().cuid(),
@@ -22,3 +19,7 @@ export const getSingleTaskSchema = z.object({
 export const deleteTaskSchema = z.object({
   taskId: z.string().cuid(),
 });
+
+// フロント側から参照される型定義
+export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>;
+export type UpdateTaskInput = z.TypeOf<typeof updateTaskShchema>;
